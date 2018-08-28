597be153cf495509c846a2ab6c2ae94eeb0626b4
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-28 18:55:23 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2_Assignment/m2/Assignment-2/Solution.java
+++ cspp2_Assignment/m2/Assignment-2/Solution.java
@@ -1,24 +1,36 @@
 import java.util.Scanner;
+/**
+* @author Rohithmarktricks
+* Date 28th August 2018
+*/
 
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/*
-	Need to write the rootsOfQuadraticEquation function and print the output.
-	*/
-	public static void rootsOfQuadraticEquation(final int x, final int y, final int z){
-		double root1 = Math.sqrt(y * y - 4 * x * z);
-		double root2 = Math.sqrt(y * y - 4 * x * z);
-		int dnm = 2 * x;
-		System.out.println((root1 - y) / dnm + " " + (-root2 - y) / dnm);
+public final class Solution {
+     /**
+    * Do not modify this main function.
+    */
+    private Solution() {
+        //empty constructor.
+    }
+     /**
+    * @param args is of string type: value taken from command prompt
+    *
+    */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
+    }
+     /**
+    * @param x,y,z takes input.
+    * 
+    */
+    public static void rootsOfQuadraticEquation(final int x, final int y, final int z){
+        double root1 = Math.sqrt(y * y - 4 * x * z);
+        double root2 = Math.sqrt(y * y - 4 * x * z);
+        int dnm = 2 * x;
+        System.out.println((root1 - y) / dnm + " " + (-root2 - y) / dnm);
 
-	}
+    }
 }
