6702a6c6704a1cea1d8f1e5b72c7adcbb435534d
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-28 19:26:07 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2_Assignment/m2/Assignment-3/Solution.java
+++ cspp2_Assignment/m2/Assignment-3/Solution.java
@@ -14,23 +14,23 @@ public final class Solution {
     * @param args takes string from command line 
     */
     public static void main(final String[] args) {
-        Scanner s=new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
-        System.out.println((long)power(base,exponent));
+        System.out.println((long)power(base, exponent));
     }
     /**
     * @param x takes input.
     * @param y takes input.
     * @return power
     */
-    public static long power(final int x, final int y){
+    public static long power(final int x, final int y) {
         if (y == 1){
             return x;
         } else if (y == 0){
             return 1;
         } else {
-            return (long)x*power(x, y-1);
+            return (long)x*power(x, y - 1);
         }
     }
 }
