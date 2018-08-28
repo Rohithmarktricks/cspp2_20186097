f1abe8b3af3349b64c61de8e762b7b8964b6c8dc
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-28 19:25:31 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2_Assignment/m2/Assignment-3/Solution.java
+++ cspp2_Assignment/m2/Assignment-3/Solution.java
@@ -3,34 +3,34 @@ import java.util.Scanner;
 * @author Rohithmarktricks
 * Date 28th August 2018
 */
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	private Solution() {
+public final class Solution {
+    /*
+    Do not modify this main function.
+    */
+    private Solution() {
         //empty constructor.
     }
     /**
     * @param args takes string from command line 
     */
-	public static void main(final String[] args) {
-		Scanner s=new Scanner(System.in);      
+    public static void main(final String[] args) {
+        Scanner s=new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
-       	System.out.println((long)power(base,exponent));
-	}
-	/**
+        System.out.println((long)power(base,exponent));
+    }
+    /**
     * @param x takes input.
     * @param y takes input.
     * @return power
     */
-	public static long power(final int x, final int y){
-		if (y == 1){
-			return x;
-		} else if (y == 0){
-			return 1;
-		} else {
-			return (long)x*power(x, y-1);
-		}
-	}
+    public static long power(final int x, final int y){
+        if (y == 1){
+            return x;
+        } else if (y == 0){
+            return 1;
+        } else {
+            return (long)x*power(x, y-1);
+        }
+    }
 }
