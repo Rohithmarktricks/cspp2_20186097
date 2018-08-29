b5855c93d2d32f25d5edf9afdf3b6339e0c094e6
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-29 12:10:37 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

---
+++ cspp2_Assignment/m3/Assignment-2/Solution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/**
+* Do not modify this main function.
+*/
+public class Solution {
+/* Fill the main function to print the number of 7's between 1 to n*/
+    /**
+    * @param args are of type:String
+    */
+    public static void main(final String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter the limit : ");    
+        int n = s.nextInt();
+        int no_sevens = 0;
+        for (int i = 0; i <= n; i++){
+        	for (char c : String.valueOf(i).toCharArray()){
+        		if(c == '7') no_sevens++;
+        	}
+        }
+        System.out.println(no_sevens);    
+    }
+}
