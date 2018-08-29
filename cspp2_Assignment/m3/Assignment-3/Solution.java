0a56e96d393b85bbfcdde088970062c0ac576e2d
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-29 13:37:49 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2_Assignment/m3/Assignment-3/Solution.java
+++ cspp2_Assignment/m3/Assignment-3/Solution.java
@@ -7,25 +7,25 @@ public class Solution {
     /**
     * class Solution.
     */
-    protected Solution(){
+    protected Solution() {
         //empty Solution constructor.
     }
     /**
     * @param args of string type.
     */
-    public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);      
-        int n1 = s.nextInt();
-        int n2 = s.nextInt();
-        System.out.println(gcd(n1,n2));
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);  
+        final int n1 = s.nextInt();
+        final int n2 = s.nextInt();
+        System.out.println(gcd(n1, n2));
     }
-    /*
-	Need to write the gcd function and print the output.
-	*/
-    public static int gcd(int x, int y){
+    /**
+    * gcd function and print the output.
+    */
+    public static int gcd(final int x, final int y) {
         int min = x < y ? x : y;
-        for (int i = min; i >= 1; i--){
-            if (x % i == 0 & y % i == 0){
+        for (int i = min; i >= 1; i--) {
+            if (x % i == 0 & y % i == 0) {
                 return i;
             }
         }
