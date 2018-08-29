b2903e7d5d467266aa43b2857c06e2bef06d67e9
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-29 13:39:01 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2_Assignment/m3/Assignment-3/Solution.java
+++ cspp2_Assignment/m3/Assignment-3/Solution.java
@@ -14,13 +14,15 @@ public class Solution {
     * @param args of string type.
     */
     public static void main(final String[] args) {
-        Scanner s = new Scanner(System.in);  
+        Scanner s = new Scanner(System.in);
         final int n1 = s.nextInt();
         final int n2 = s.nextInt();
         System.out.println(gcd(n1, n2));
     }
     /**
-    * gcd function and print the output.
+    * @param x int input.
+    * @param y int input.
+    * @return int type
     */
     public static int gcd(final int x, final int y) {
         int min = x < y ? x : y;
