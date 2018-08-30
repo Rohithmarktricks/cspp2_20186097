caaa88fcdc0bdde6e7b3d28698d9e452f29cd1c1
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-30 19:11:00 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2_Assignment/m4/Assignment-2/Solution.java
+++ cspp2_Assignment/m4/Assignment-2/Solution.java
@@ -10,28 +10,34 @@ public class Solution {
         //empty Constructor.
     }
     /**
+     * Creates a matrix.
+     *
+     * @param      scan  The scan
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static int[][] createMatrix(Scanner scan) {
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+            int[][] arr1 = new int[x][y];
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++){
+                arr1[i][j] = scan.nextInt();
+            }
+        }
+        return arr1;
+    }
+    /**
      * Main method.
      *
      * @param      args  The arguments.
      */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int x = scan.nextInt();
-        int y = scan.nextInt();
-        int[][] arr1 = new int[x][y]; 
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++){
-                arr1[i][j] = scan.nextInt();
-            }
-        }
-        int z = scan.nextInt();
-        int v = scan.nextInt();
-        int[][] arr2 = new int[z][v];
-        for (int i = 0; i < z; i++) {
-            for (int j = 0; j < v; j++){
-                arr2[i][j] = scan.nextInt();
-            }
-        }
+        int[][] arr1 = createMatrix(scan); 
+        int[][] arr2 = createMatrix(scan);
+        int x = arr1.length, z = arr2.length;
+        int y = arr1[0].length, v = arr2[0].length;
         if (x == z & y == v) {
             int result = 0;
             String temp = "";
