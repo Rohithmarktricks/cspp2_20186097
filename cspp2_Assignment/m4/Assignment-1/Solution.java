43d9287bd75a19ceb661b97f0399524114b39765
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-30 15:49:06 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2_Assignment/m4/Assignment-1/Solution.java
+++ cspp2_Assignment/m4/Assignment-1/Solution.java
@@ -2,27 +2,26 @@ import java.util.*;
 /**
 * Class for solution.
 */
-public class Solution
+public final class Solution
 {
     /**
      * Constructs the object.
      */
-    private Solution(){
+    private Solution() {
         //empty Constructor.
     }
     /**
     * @param args are of string type.
     * prints the maximum of array elements. 
     */
-    public static void main(final String[] args)
-    {
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int max = 0;
         final int size = sc.nextInt();
         int[] num = new int[size];
         for(int i = 0; i < size ; i++) {
             num[i] = sc.nextInt();
-            if (max < num[i]){
+            if (max < num[i]) {
                 max = num[i];
             }
         }
