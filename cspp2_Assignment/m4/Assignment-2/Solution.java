a5be9376cb3f38509608507a5d7ff593cc7560a8
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-30 19:05:33 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2_Assignment/m4/Assignment-2/Solution.java
+++ cspp2_Assignment/m4/Assignment-2/Solution.java
@@ -27,23 +27,23 @@ public class Solution {
         int z = scan.nextInt();
         int v = scan.nextInt();
         int[][] arr2 = new int[z][v];
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++){
+        for (int i = 0; i < z; i++) {
+            for (int j = 0; j < v; j++){
                 arr2[i][j] = scan.nextInt();
             }
         }
         if (x == z & y == v) {
-        int result = 0;
-        String temp = "";
-        for (int k = 0; k < x; k++) {
-            for (int l = 0; l < y; l++){
-                result = arr1[k][l] + arr2[k][l];
-                temp += result + " ";
+            int result = 0;
+            String temp = "";
+            for (int k = 0; k < x; k++) {
+                for (int l = 0; l < y; l++){
+                    result = arr1[k][l] + arr2[k][l];
+                    temp += result + " ";
+                }
+                temp =temp.trim();
+                temp += "\n";
             }
-            temp =temp.trim();
-            temp += "\n";
-        }
-        System.out.println(temp);
+            System.out.println(temp);
         } else {
             System.out.println("not possible");
         }
