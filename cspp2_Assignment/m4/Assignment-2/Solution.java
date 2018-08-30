ccd5306652bd78ec42034bfa7443b426f5d3faa8
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-30 18:51:26 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2_Assignment/m4/Assignment-2/Solution.java
+++ cspp2_Assignment/m4/Assignment-2/Solution.java
@@ -3,45 +3,43 @@ import java.util.Scanner;
 * Fill the main function to print resultant of addition of matrices.
 */
 public class Solution {
-	/**
-	* Constructs the object.
-	*/
-	protected Solution() {
-		//empty Constructor.
-	}
-	protected static int[][] matrix(int x, int y){
-		int num = 0;
-		Scanner scan = new Scanner(System.in);
-		int[][] arr = new int[x][y];
-		for (int i = 0; i < x; i++) {
-			for (int j = 0; j < y; j++){
-				num = scan.nextInt();
-				arr[i][j] = num;
-			}
-		}
-		return arr;
-	}
-	/**
-	 * Main method.
-	 *
-	 * @param      args  The arguments.
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int x = scan.nextInt();
-		int y = scan.nextInt();
-		int[][] arr1 = matrix(x,y);
-		int z = scan.nextInt();
-		int v = scan.nextInt();
-		int[][] arr2 = matrix(z,v);
-		//int[][] result = new int[x][y];
-		int result = 0;
-		for (int k = 0; k < x; k++) {
-			for (int l = 0; l < y; l++){
-				result = arr1[k][l] + arr2[k][l];
-				System.out.format("%d ",result);
-			}
-			System.out.format("\n");
-		}
-	}
+    /**
+    * Constructs the object.
+    */
+    protected Solution() {
+        //empty Constructor.
+    }
+    protected static int[][] matrix(int x, int y){
+        Scanner scan1 = new Scanner(System.in);
+        int[][] arr = new int[x][y];
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++){
+                arr[i][j] = scan1.nextInt();
+            }
+        }
+        return arr;
+    }
+    /**
+     * Main method.
+     *
+     * @param      args  The arguments.
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+        int[][] arr1 = matrix(x,y);
+        int z = scan.nextInt();
+        int v = scan.nextInt();
+        int[][] arr2 = matrix(z,v);
+        //int[][] result = new int[x][y];
+        int result = 0;
+        for (int k = 0; k < x; k++) {
+            for (int l = 0; l < y; l++){
+                result = arr1[k][l] + arr2[k][l];
+                System.out.format("%d ",result);
+            }
+            System.out.format("\n");
+        }
+    }
 }
