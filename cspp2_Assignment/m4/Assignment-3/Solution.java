efa7d51837e7398a8b71f8fe43e0ad39fb9b1837
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-31 11:04:51 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2_Assignment/m4/Assignment-3/Solution.java
+++ cspp2_Assignment/m4/Assignment-3/Solution.java
@@ -1,42 +1,48 @@
-
-// Given an String, Write a java method that returns the decimal value for the given binary string.
-
-
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 public class Solution {
 
+    /**
+     * Constructs the object.
+     */
+    protected Solution(){
+        //empty Constructor.
+    }
 
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		//sc.nextLine();
-		for (int i = 0; i < n; i++) {
-			String s = sc.next();
-			String res = binaryToDecimal(s);//Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-	/**
-	 * binaryToDecimal method.
-	 * @param str takes a binary string.
-	 */
 
-	final static String binaryToDecimal(final String str) {
-		int sum = 0;
-		int i = 1;
-		for (int j = str.length() - 1; j >= 0; j--) {
-			if (str.charAt(j) == '1') {
-				sum += i;
-				i *= 2;
-			} else {
-				i *= 2;
-			}
-		}
-		return String.valueOf(sum);
-	}
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            String res = binaryToDecimal(s); 
+            System.out.println(res);
+        }
+    }
+    /**
+     * binaryToDecimal method.
+     * @param str takes a binary string.
+     */
+
+    final static String binaryToDecimal(final String str) {
+        int sum = 0;
+        int i = 1;
+        for (int j = str.length() - 1; j >= 0; j--) {
+            if (str.charAt(j) == '1') {
+                sum += i;
+                i *= 2;
+            } else {
+                i *= 2;
+            }
+        }
+        return String.valueOf(sum);
+    }
 }
