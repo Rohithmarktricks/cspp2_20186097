f5e66a337bf6eaf6bafd1b1f58b8e21bae665fb8
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-08-31 11:01:14 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2_Assignment/m4/Assignment-3/Solution.java
+++ cspp2_Assignment/m4/Assignment-3/Solution.java
@@ -6,27 +6,27 @@ import java.util.Scanner;
 public class Solution {
 
 
-/**
- * { function_description }
- *
- * @param      args  The arguments
- */
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		//sc.nextLine();
-		for(int i=0; i<=n; i++) {
+		for (int i = 0; i < n; i++) {
 			String s = sc.next();
 			String res = binaryToDecimal(s);//Write binaryToDecimal function
 			System.out.println(res);
 		}
 	}
 
-	final static String binaryToDecimal(String str){
+	final static String binaryToDecimal(String str) {
 		int sum = 0;
 		int i = 1;
-		for(int j = str.length() - 1; j >= 0; j--) {
-			if (str.charAt(j) == '1'){
+		for (int j = str.length() - 1; j >= 0; j--) {
+			if (str.charAt(j) == '1') {
 				sum += i;
 				i *= 2;
 			} else {
@@ -35,5 +35,5 @@ public class Solution {
 			}
 		}
 		return String.valueOf(sum);
-}
+	}
 }
