edfd126a0b2468b9e63e92cf9d5b71af4bbc5603
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-09-07 17:35:25 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2_Assignment/m11/Assignment-1/List.java
+++ cspp2_Assignment/m11/Assignment-1/List.java
@@ -441,7 +441,7 @@ public class List {
                     l.removeAll(a);
                 }
                 break;
-            case "subList": {
+            case "subList": 
                 if (tokens.length != 2) {
                     break;
                 }
@@ -452,12 +452,12 @@ public class List {
                     System.out.println(object);
                 }
                 break;
-            }
+            
             case "equals":
                 if (tokens.length == 2) {
                     String[] lt = tokens[1].split(",");
                     List l2 = new List();
-                    for (int k = 0; k < lt.length; k++ ) {
+                    for (int k = 0; k < lt.length; k++) {
                         l2.add(Integer.parseInt(lt[k]));
                     }
                     System.out.println(l.equals(l2));
